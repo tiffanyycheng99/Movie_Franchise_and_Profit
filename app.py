bca18f1bde9919b015763a3541ec79abdf90be81
Kyla <75227767+kylagelev@users.noreply.github.com>
2021-05-23 22:23:55 -0700

    flask rendering templates

--- app.py
+++ app.py
@@ -1,36 +1,43 @@
-import sqlalchemy
-from sqlalchemy.ext.automap import automap_base
-from sqlalchemy.orm import Session
-from sqlalchemy import create_engine, func
-import json
-from flask import Flask
+# import sqlalchemy
+# from sqlalchemy.ext.automap import automap_base
+# from sqlalchemy.orm import Session
+# from sqlalchemy import create_engine, func
+# import json
+from flask import Flask, render_template
 
-eng = create_engine("postgresql://postgres:postgres@movie-swear-db.cfgivq9r1u3j.us-west-2.rds.amazonaws.com:5432/moviesweardb")
-con = eng.connect()
-Base = automap_base()
-Base.prepare(eng, reflect=True)
-movieSwear = Base.classes.movieSwear
+# eng = create_engine("postgresql://postgres:postgres@movie-swear-db.cfgivq9r1u3j.us-west-2.rds.amazonaws.com:5432/moviesweardb")
+# con = eng.connect()
+# Base = automap_base()
+# Base.prepare(eng, reflect=True)
+# movieSwear = Base.classes.movieSwear
 
-session = Session(eng)
+# session = Session(eng)
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-        return (f"Welcome to the Data of Movies and Swear Words.<br/><br/>"
-                "Available Route:<br/>" 
-                "/api/v1.0/get_movie_swear<br/>")
+        return render_template("index.html")
 
-@app.route("/api/v1.0/get_movie_swear")
-def get_movie_swear():
-        session = Session(eng)
-        results = session.execute('SELECT * FROM "movieSwear"')
-        response = [dict(row.items()) for row in results]
-        all_results = json.dumps(response)
+@app.route("/api/v1.0/timeseries_viz")
+def timeseries_viz():
+        return render_template("timeseries_viz.html")
 
-        session.close()
-        movie_swear_json = all_results
-        return(movie_swear_json)
+@app.route("/api/v1.0/contentrating_viz")
+def contentrating_viz():
+        return render_template("contentrating_viz.html")
+
+@app.route("/api/v1.0/content_rating_ML")
+def content_rating_ML():
+        return render_template("content_rating_ML.html")
+
+@app.route("/api/v1.0/breakeven_ML")
+def breakeven_ML():
+        return render_template("breakeven_ML.html")
+
+@app.route("/api/v1.0/bonus_visuals")
+def bonus_visuals():
+        return render_template("bonus_visuals.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
